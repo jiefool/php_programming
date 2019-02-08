8fc471c221d23d6163c3963a849b5c0cb44d3b77
Jay Paul Aying <jaypaulaying5@gmail.com>
2019-02-08 10:44:03 +0800

    initial files

---
+++ form.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <form action="save_to_csv.php" method="get">
+      <label>First name:</label>
+      <input type="text" name="first_name"><br/>
+      <label>Last name:</label>
+      <input type="text" name="last_name"><br/>
+      <button type="submit">Submit</button>
+    </form>
+  </body>
+</html>
